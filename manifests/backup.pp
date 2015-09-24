eba171e712791d5d438ddea7c8715016ac565e58
Lebedev Vadim <abraham1901@gmail.com>
2015-09-24 15:52:02 +0300

    * Add bacula backup manifest

---
+++ manifests/backup.pp
@@ -0,0 +1,128 @@
+class pp::backup (
+  $mysql          = false,
+  $bacula_client  = true,
+  $director_password, 
+  $director_server,
+  $storage_server,
+  $backup_files = undef,
+) {
+  if $bacula_client {
+
+    class { 'bacula':
+      director_password => $director_password,
+      director_server   => $director_server,
+      is_client         => true,
+      storage_server    => $storage_server,
+    }
+
+    @@bacula::client::config { $::fqdn:
+      client_schedule   => 'daily-schedule200',
+      director_password => $director_password,
+      director_server   => $director_server,
+      fileset           => "${fqdn}_files",
+      storage_server    => $storage_server,
+      run_scripts         => [ {
+        'Command'       => '/usr/bin/vbackup 0',
+        'RunsOnClient'  => 'Yes',
+        'RunsWhen'      => 'Before',
+     }, {
+        'Command'       => 'rm -Rf /tmp/bacula',
+        'RunsOnClient'  => 'Yes',
+        'RunsWhen'      => 'After',
+     } ]
+    }
+
+    if $backup_files != undef {
+      $tmp_backup_files = $backup_files + [ '/tmp/bacula/' ]
+    } else {
+      $tmp_backup_files = [ '/tmp/bacula/' ]
+    }
+
+    validate_array($tmp_backup_files)
+
+     @@bacula::director::fileset { "${fqdn}_files":
+      include_files => $tmp_backup_files
+    }
+  }
+
+  package { 'vbackup':
+    ensure => installed
+  }
+
+  file { '/etc/vbackup/backup.0':
+    ensure => directory,
+    require => Package[ 'vbackup' ],
+  }
+
+  file { '/etc/vbackup/backup.0/20-dpkg.dpkg':
+    ensure  => present,
+    content => 'DESTDIR="dpkg/"',
+    mode  => '0600',
+    owner => 'root',
+  }
+
+  if $::virtual == 'physical' {
+    file { '/etc/vbackup/backup.0/25-mbr.mbr':
+      ensure  => present,
+      content => 'DISKS=""
+DESTDIR="mbr/"
+DESTFILE="mbrs.%D1%"',
+      mode  => '0600',
+      owner => 'root',
+    }
+  } else {
+      file { '/etc/vbackup/backup.0/25-mbr.mbr':
+        ensure => absent,
+      }
+    
+  
+  }
+
+
+  if $mysql {
+    file { '/etc/vbackup/backup.0/30-mysql.mysql':
+      ensure  => present,
+      content => "PASSWORD=\"${::admin_password}\"
+MYUSER=\"root\"
+DATABASES=\"-\"
+DESTDIR=\"mysql/%D1%\"",
+      mode  => '0600',
+      owner => 'root',
+    }
+  }
+
+  file { '/etc/vbackup/backup.0/50-tar.tar':
+    ensure  => present,
+    content => 'DIRS=\'/var/spool/cron/crontabs/ /etc /boot\'
+LEVEL=0
+STATEDIR="/var/lib/vbackup/state/tar"
+DESTDIR="fs/%D1%"
+',
+    mode  => '0600',
+    owner => 'root',
+  }
+
+  file { '/etc/vbackup/backup.0/vbackup.conf':
+    ensure  => present,
+    content => '# Per backup global configuration file
+#
+# This file holds per-backup configuration options
+
+# A directory where backups will be stored
+# All other DESTDIR directives are relative to this one
+DESTDIR0=/tmp/bacula/
+
+# Compress backups? (yes/no)
+COMPRESS="no"
+',
+    mode  => '0600',
+    owner => 'root',
+  }
+
+
+
+}
+
+
+
+
